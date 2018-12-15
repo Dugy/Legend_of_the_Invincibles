efbe43fd1f847f40e1109a02bfc12665c65b2b43
Dugy <jan.dugacek@gmail.com>
2018-12-15 16:02:56 +0100

    Moved the debugging function into a new file

--- lua/lua.lua
+++ lua/lua.lua
@@ -1,3 +1,4 @@
+wesnoth.dofile("~add-ons/Legend_of_the_Invincibles/lua/debug.lua")
 wesnoth.dofile("~add-ons/Legend_of_the_Invincibles/lua/titles.lua")
 wesnoth.dofile("~add-ons/Legend_of_the_Invincibles/lua/redeem.lua")
 
