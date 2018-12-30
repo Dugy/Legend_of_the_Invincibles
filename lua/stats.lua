5f2af9d8f24446220c7816ca472eabf5ede5cbaf
Edward Chernenko <edwardspec@gmail.com>
2018-12-31 01:29:55 +0300

    stats.lua: skip defense adjustment for items that give no defense bonus
    This resolves #203.

--- lua/stats.lua
+++ lua/stats.lua
@@ -133,11 +133,9 @@ function wesnoth.update_stats(original)
 				local sort = mods[i][2].sort
 				mods[i][2] = wesnoth.deepcopy(item_list[mods[i][2].number])
 
-				if mods[i][2].sort == "armourword" then
-					if sort == "helm" or sort == "boots" or sort == "gauntlets" then
-						-- Crafted non-armours have only 1/3 of the defence of crafted armours.
-						mods[i][2].defence = mods[i][2].defence / 3
-					end
+				if mods[i][2].sort == "armourword" and sort ~= "armour" and mods[i][2].defence then
+					-- Crafted non-armours have only 1/3 of the defence of crafted armours.
+					mods[i][2].defence = mods[i][2].defence / 3
 				end
 
 				mods[i][2].sort = sort
