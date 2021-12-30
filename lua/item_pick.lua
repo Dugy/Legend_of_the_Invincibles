f210346ac671f164b3d4b21fabe4bf13eac8659f
Edward Chernenko <edwardspec@gmail.com>
2021-12-31 00:28:46 +0300

    (luacheck) Fix luacheck warnings "shadowing upvalue"
    For example, item_pick_menu() had two variables named "count",
    one inside show_picking_dialogue(), one outside of it,
    which is confusing.

--- lua/item_pick.lua
+++ lua/item_pick.lua
@@ -6,6 +6,53 @@ local _ = wesnoth.textdomain "wesnoth-loti"
 local item_picker = {} -- for its own mp safety queue
 wesnoth.require("inventory/multiplayer_safety")(item_picker)
 
+--- actions:
+--- 0 is equip/use, 1 is to store, 2 is to destroy, 3 is to leave on the ground
+local function show_picking_dialogue(item, sort, replaced_item, cant_equip, count)
+	local description = loti.item.describe_item(item.number, sort)
+	if item.sort == "potion" then
+		local res = gui.show_narration ({
+			title = item.name,
+			portrait = item.image,
+			message = description .. "\n\n" .. _"Use this potion?"
+		}, {_"Use it.", _"Store it. Currently having "..count.._" such items stored.", _"Leave it on the ground."})
+		if res == 1 then
+			return 0
+		elseif res == 2 then
+			return 1
+		else
+			return 3
+		end
+	end
+
+	if cant_equip then
+		local res = gui.show_narration ({
+			title = item.name,
+			portrait = item.image,
+			message = description .. "\n\n" .. "<span color='red'>" .. cant_equip .. "</span>"
+		}, {_"Store it. Currently having "..count.._" such items stored.", _"Smash it to get a random gem.", _"Leave it on the ground."})
+		if res == 1 then
+			return 1
+		elseif res == 2 then
+			return 2
+		else
+			return 3
+		end
+	end
+
+	--in case we can equip
+	local res = gui.show_narration ({
+			title = item.name,
+			portrait = item.image,
+			message = description .. "\n\n" .. _ "Item of the same type that will be unequipped: " .. replaced_item .. "\n" .. _"Take this item?"
+	}, {_"Take it.", _"Store it. Currently having "..count.._" such items stored.",_"Smash it to get a random gem.", _"Leave it on the ground."})
+	if (res >= 1) and (res <= 4) then
+		return res - 1
+	else
+		return 3
+	end
+end
+
 function loti.util.item_pick_menu (mpsafety, unit)
 	local hex_items = loti.item.on_the_ground.list_with_sorts (unit.x, unit.y)
 --
@@ -41,51 +88,7 @@ function loti.util.item_pick_menu (mpsafety, unit)
 			if this_sort[number] then
 				count = this_sort[number]
 			end
-			--- actions:
-			--- 0 is equip/use, 1 is to store, 2 is to destroy, 3 is to leave on the ground
-			local show_picking_dialogue = function(item, sort, replaced_item, cant_equip, count)
-				local description = loti.item.describe_item(item.number, sort)
-				if item.sort == "potion" then
-					local res = gui.show_narration ({
-						title = item.name,
-						portrait = item.image,
-						message = description .. "\n\n" .. _"Use this potion?"
-					}, {_"Use it.", _"Store it. Currently having "..count.._" such items stored.", _"Leave it on the ground."})
-					if res == 1 then
-						return 0
-					elseif res == 2 then
-						return 1
-					else
-						return 3
-					end
-				end
-				if cant_equip then
-					local res = gui.show_narration ({
-						title = item.name,
-						portrait = item.image,
-						message = description .. "\n\n" .. "<span color='red'>" .. cant_equip .. "</span>"
-					}, {_"Store it. Currently having "..count.._" such items stored.", _"Smash it to get a random gem.", _"Leave it on the ground."})
-					if res == 1 then
-						return 1
-					elseif res == 2 then
-						return 2
-					else
-						return 3
-					end
-				end
-				--in case we can equip
-				local res = gui.show_narration ({
-						title = item.name,
-						portrait = item.image,
-						message = description .. "\n\n" .. _ "Item of the same type that will be unequipped: " .. replaced_item .. "\n" .. _"Take this item?"
-				}, {_"Take it.", _"Store it. Currently having "..count.._" such items stored.",_"Smash it to get a random gem.", _"Leave it on the ground."})
-				if (res >= 1) and (res <= 4) then
-					return res - 1
-				else
-					return 3
-				end
 
-			end
 			local result = show_picking_dialogue(item, sort, replaced_item, why_cant_equip, count)
 			if result == 0 then
 				mpsafety:queue({
