c032ebadfbcab16be945cd935806c74253a87bdf
white-haired-uncle <larry@hawkeyes.org>
2023-07-22 17:35:46 -0400

    updated lua trader

--- lua/trader.lua
+++ lua/trader.lua
@@ -16,10 +16,170 @@ local function dump(o)
    end
 end
 
+local function confirm_purchase(item,trader)
+	-- Prepare the layout structure for gui.show_dialog().
+	local listbox_id = "trader_menu_confirm"
 
+	-- 1) listbox_template: shows one possible choice (yes, no)
+	local listbox_template = wml.tag.grid {
+		wml.tag.row {
+			wml.tag.column {
+				grow_factor = 1,
+				border = "all",
+				border_size = 0,
+				horizontal_alignment = "center",
+				wml.tag.label {
+					id = "option"
+				}
+			}
+		}
+	}
+
+	-- 2) listbox_widget: shows a list of all options
+	local listboxDefinition = wml.tag.listbox {
+		id = listbox_id,
+		wml.tag.list_definition {
+			wml.tag.row {
+				wml.tag.column {
+					horizontal_grow = true,
+					wml.tag.toggle_panel { listbox_template }
+				}
+			}
+		}
+	}
+
+
+	-- 3) Top-level dialog, includes help message and listbox.
+	local confirm_queries = { _"A bargain at twice the price.  We good?", _"Makes a great gift.  Wrap it up to go?", _"You gonna buy that or what?",
+		_"I close in 5 minutes.  We got a deal or what?", _"Look at it.  Do you really want it?" }
+	local confirm_query=confirm_queries[math.random(1,#confirm_queries)]
+	if(string.sub(item.image,7,10) == "book") then confirm_query = _"Hey, this ain't a library!  You wanna take that home to read?" end
+	local dialogDefinition = {
+		wml.tag.tooltip { id = "tooltip_large" },
+		wml.tag.helptip { id = "tooltip_large" },
+		wml.tag.grid {
+			wml.tag.row {
+				wml.tag.column {  -- column 1, item
+					border = "all",
+
+					border_size=50,
+					wml.tag.grid {
+						wml.tag.row { -- item image and name
+							wml.tag.column {
+								wml.tag.grid {
+									wml.tag.row {
+										wml.tag.column {
+											border = "bottom",
+											border_size=10,
+											wml.tag.image {
+												label = item.image
+											}
+										},
+										wml.tag.column {
+											wml.tag.label {
+												label = item.name .. "  (" .. item.sort .. ")"
+											}
+										},
+									}
+								}
+							}
+						},
+						wml.tag.row { -- item description
+							wml.tag.column {
+								wml.tag.label {
+									use_markup = true,
+									label = item.description
+								}
+							}
+						}
+					}
+				},
+				wml.tag.column {  -- column2, let's do business
+						wml.tag.grid {
+							wml.tag.row { -- the trader
+								wml.tag.column {
+									wml.tag.grid {
+										wml.tag.row {
+											wml.tag.column {
+												wml.tag.label {
+													border = "bottom",
+													border_size=30,
+													use_markup = true,
+													label = "<span size='large' color='wheat' weight='bold'>".. trader .."</span>"
+												}
+											},
+											wml.tag.column {
+												wml.tag.image {
+													label = "merchant.png"
+												}
+											}
+										}
+									}
+								}
+							},
+							wml.tag.row {  -- you want it?
+								wml.tag.column {
+										wml.tag.label {
+										use_markup = true,
+										label = "<span color='wheat' weight='bold'>".. confirm_query .."</span>"
+									}
+								}
+							},
+							wml.tag.row {  -- blank space
+								wml.tag.column {
+									wml.tag.label {
+										label = "   "
+									}
+								}
+							},
+							wml.tag.row {  -- the options (yes, no), in a listbox
+								wml.tag.column { horizontal_grow = true, listboxDefinition }
+							},
+							wml.tag.row {  -- the okay button
+								wml.tag.column {
+									border = "top",
+									border_size = 10,
+									wml.tag.button { id = "ok", label = _"OK" },
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+	local options = {}
+	table.insert(options,{label = _"Yes", ret_val = true})
+	table.insert(options,{label = _"No", ret_val = false})
+
+	local ret_val=false
+
+	local function get_selected_option(dialog)
+		local index = dialog[listbox_id].selected_index
+		return options[index]
+	end
+
+	local function preshow(dialog)
+		local listbox = dialog[listbox_id]
+		for index, option in ipairs(options) do
+			listbox[index].option.label = option.label
+		end
+
+		listbox:focus()
+	end
+
+	local function postshow(dialog)
+		local selected_option = get_selected_option(dialog)
+		ret_val = selected_option.ret_val
+	end
+
+	gui.show_dialog(dialogDefinition, preshow, postshow)
+
+	return ret_val
+end
 
 local function trader_menu(cfg)
-	local debug = true
+	local debug = false
 	local x = cfg.x
 	local y = cfg.y
 	local items = {}
@@ -28,17 +188,22 @@ local function trader_menu(cfg)
 		items=loti.item.type.numbers_between(521,530)  -- Add gems.  Looking at ALL items for sort=gem might be wiser.
 		for w in string.gmatch(cfg.items, "[%d]+") do table.insert(items,tonumber(w)) end
 	end
-	local trader_id=cfg.id
-
-	local gold = wesnoth.sides.get(1).gold  -- that's side=1, if it matters (MP?)
+	local unique_id=cfg.unique_id  -- Unique id for this trader, used to avoid purchasing same item more than once per trader
+	local trader=cfg.trader
+	local bought_items = {}
+	if (cfg.bought_items ~= nil) then bought_items = cfg.bought_items end
+	local gold = cfg.gold
+	local side_number = cfg.side_number
+	print("Gold = " .. gold)
 
 	-- Menu items, as expected by gui.show_menu()
 	local menu_items = {}
-	--table.insert(menu_items, {image="",details="<span weight='bold'>Nothing</span>",selected_item=nil,allowed=true})
-	table.insert(menu_items, {image="",details="Nothing",selected_item=nil,allowed=true})
+	table.insert(menu_items, {image="",use_markup="yes",details="<span weight='bold'>Nothing</span>",selected_item=nil,allowed=true})
 
 	for _,id in pairs(items) do
 		(function()
+			local _ = wesnoth.textdomain "wesnoth-loti"
+
 			local item=loti.item.type[id]
 			if (item.cost == nil) then
 				if (debug) then print(string.format("item %d has no assigned cost, check item number and util/item_list.cfg",id)) end
@@ -48,35 +213,31 @@ local function trader_menu(cfg)
 				local trader_buys = wml.array_access.get "trader_buys"
 				if (debug) then print("trader_buys is a " .. type(trader_buys)) end
 				if(type(trader_buys) == "table") then
-					for i,k in pairs(trader_buys) do
-						print(string.format("i = <%s>, type(k) = <%s>",i,type(k)))
-					end
-					local var="trader_buys."..trader_id.."."..id
+					local var="trader_buys."..unique_id.."."..id
 					local bought_it=wml.variables[var]
 					if (bought_it) then return end
 				end
 			end
 			if (debug) then print(string.format("id = %d, name = %s, cost = %d, image = %s",id,item.name,item.cost,item.image)) end
 
-			local label = string.format("%s (%d gold)",item.name,item.cost)
+			local label = item.name .. "(" .. item.cost .. _ " gold" .. ")"
 			if (item.sort == "gem") then
-				local prefix="Buy a "
-				if (string.find(string.sub(tostring(item.name),1,1),"[AEIOUaeiou]")) then prefix="Buy an " end
 				local have = wml.variables[item.wml_var]
 				if (have == nil) then have=0 end
-				label=prefix .. string.lower(tostring(item.name)) .. " for " .. item.cost .. " (currently have " .. have .. " of them)"
+				label=item.offer .. " " .. tostring(item.cost) .. _ " (currently have " .. have .. _ " of them)"
 			end
 
 			local allowed = true
 			if (item.cost > gold) then allowed = false end
 
 			local image=item.image
-			if (item.sort == "gem") then item.image=item.image .. "~CROP(18,18,36,36)" end
+			if (item.sort == "gem") then image = item.image .. "~CROP(18,18,36,36)" end
 
 			-- Might want to check here if item already in menu_items and return if so.
 			table.insert(menu_items, {
 				-- Fields with the same meaning as in gui.show_menu()
 				image = image,
+				use_markup = false,
 				details = label,
 				-- This field is used to determine which item has been selected.
 				selected_item = id,
@@ -90,7 +251,7 @@ local function trader_menu(cfg)
 	-- Prepare the layout structure for gui.show_dialog().
 	local listbox_id = "trader_menu_list"
 
-	-- 1) listbox_template: shows one possible upgrade (icon + description), e.g. Particle Storm.
+	-- 1) listbox_template: shows one possible item (e.g. Ruby, or Holy Sword)
 	local listbox_template = wml.tag.grid {
 		wml.tag.row {
 			wml.tag.column {
@@ -172,6 +333,7 @@ local function trader_menu(cfg)
 
 		for index, menu_item in ipairs(menu_items) do
 			listbox[index].image.label = menu_item.image
+			listbox[index].item_name.use_markup = menu_item.use_markup
 			listbox[index].item_name.label = menu_item.details
 
 			if not menu_item.allowed then
@@ -207,106 +369,51 @@ local function trader_menu(cfg)
 			local var=wml.variables[item.wml_var]
 			var = var + 1
 			wml.variables[item.wml_var] = var
+			wml.fire("gold",{side=side_number,amount=-item.cost})
+			gold = gold - item.cost
+			cfg.gold = gold
 			return trader_menu(cfg)  -- stay in menu until player chooses "Nothing"
 		end
 
-		-- Do you really want it? gui goes here --
+		-- Do you really want it?
+		local doit=confirm_purchase(item,trader)
+		if not doit then return trader_menu(cfg) end
 
-		wml.fire("gold",{side=1,amount=-item.cost})
+		table.insert(bought_items,item)
+		gold = gold - item.cost
 
 		if (item.sort ~= "gem") then -- item cannot be purchased more than once at this trader
-			local bought="trader_buys."  .. trader_id .. "." .. selected_menu_item.selected_item
+			local bought="trader_buys."  .. unique_id .. "." .. selected_menu_item.selected_item
 			wml.variables[bought]="yes"
 		end
-		wesnoth.wml_actions.item {  -- draw item on ground
-			x = x,
-			y = y,
-			image = item.image,
-			halo = loti.item.halo
-		}
-		loti.item.on_the_ground.add(selected_menu_item.selected_item, x, y, item.sort)  -- place item on ground
-		wml.tag.fire_event {  -- pick up item on ground
-			name = "item_pick",
-			wml.tag.primary_unit {
-				x = x,
-				y = y
-			}
-		}
-if (false) then
-		-- create event to pick up item
-		--
-		-- this section may be unnecessary, just need to make sure item_pick is there and fire?
-		--
-		-- What about units that can't equip items, what if they buy?
-		--
-	       if wml.variables["allied_sides"] then
-			wesnoth.add_event_handler {
-				id = "ie" .. x .. "|" .. y,
-				name = "moveto",
-				first_time_only = "no",
-				wml.tag.filter {
-					x = x,
-					y = y,
-					side = wml.variables["allied_sides"],
-					wml.tag["not"] {
-						wml.tag.filter_wml {
-							wml.tag.variables {
-								cant_pick = "yes"
-							}
-						}
-					},
-				},
-				wml.tag.fire_event {
-					name = "item_pick",
-					wml.tag.primary_unit {
-						x = x,
-						y = y
-					}
-				}
-			}
-		else
-        -- Enable "pick item" event when some unit walks onto this hex.
-        -- (see PLACE_ITEM_EVENT for WML version)
-        -- this is a LEGACY version, which uses the "controller" side filter
-			wesnoth.add_event_handler {
-				id = "ie" .. x .. "|" .. y,
-				name = "moveto",
-				first_time_only = "no",
-				wml.tag.filter {
-					x = x,
-					y = y,
-					wml.tag["not"] {
-						wml.tag.filter_wml {
-							wml.tag.variables {
-								cant_pick = "yes"
-							}
-						}
-					},
-					wml.tag.filter_side {
-						controller = "human"
-					}
-				},
-				wml.tag.fire_event {  -- pick up item on ground
-					name = "item_pick",
-					wml.tag.primary_unit {
-						x = x,
-						y = y
-					}
-				}
-			}
-		end
-		-- Set WML global trader_purcashed.trader_id.item
-		--
-		-- this is odd, you can keep buying items but you don't pick them up until you're done - MP?
-		-- And the first time you buy you don't get to pick it up that turn unless you use pick up items on the ground.
-		-- After that, it's immediate.
-end  -- if (false)
+		cfg.bought_items = bought_items
+		cfg.gold = gold
 		return trader_menu(cfg)  -- stay in menu until player chooses "Nothing"
-	end
---	return selected_menu_item.selected_item
+        end
+	return bought_items
 end
 
 function wesnoth.wml_actions.trader_menu(cfg)
-	trader_menu(cfg)
+	local newcfg={}
+	for index, value in pairs(cfg) do
+		newcfg[index] = value
+	end
+	local side_number = wesnoth.sides.find{ wml.tag.has_unit { x=cfg.x,y=cfg.y }}[1].side
+	local gold = wesnoth.sides.get(side_number).gold
+	print(type(side_number))
+	print(type(gold))
+	newcfg.gold=gold
+	newcfg.side_number = side_number
+
+	--local bought_items=wesnoth.synchronize_choice(trader_menu(newcfg))
+	local bought_items=wesnoth.synchronize_choice(trader_menu)
+	--local bought_items=trader_menu(newcfg)
+
+	for _, item in pairs(bought_items) do
+		--print("I bought " .. item.name)
+		wml.fire("gold",{side=side_number,amount=-item.cost})
+		loti.item.on_the_ground.add(item.number, cfg.x, cfg.y, item.sort)  -- place item on ground
+		wesnoth.wml_actions.fire_event{ name="item_pick", { "primary_unit", { x=cfg.x, y=cfg.y } } }
+	end
 end
 
