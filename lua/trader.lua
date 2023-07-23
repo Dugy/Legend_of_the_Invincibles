3fdb3c98297b8f8d3c376a239713d933dd4fecbe
white-haired-uncle <larry@hawkeyes.org>
2023-07-23 18:39:56 -0400

    updated lua trader

--- lua/trader.lua
+++ lua/trader.lua
@@ -1,22 +1,6 @@
 --! #textdomain "wesnoth-loti"
 local _ = wesnoth.textdomain "wesnoth-loti"
 
---luacheck: ignore 211
---luacheck: ignore 511
---luacheck: ignore 241
-local function dump(o)
-   if type(o) == 'table' then
-      local s = '\n{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
 local function confirm_purchase(item,trader)
 	-- Prepare the layout structure for gui.show_dialog().
 	local listbox_id = "trader_menu_confirm"
@@ -129,6 +113,8 @@ local function confirm_purchase(item,trader)
 							wml.tag.row {  -- blank space
 								wml.tag.column {
 									wml.tag.label {
+										border = "all",
+										border_size = 10,
 										label = "   "
 									}
 								}
@@ -149,6 +135,7 @@ local function confirm_purchase(item,trader)
 			}
 		}
 
+		-- Possible alternative gui.show_prompt https://wiki.wesnoth.org/LuaAPI/gui ???
 	local options = {}
 	table.insert(options,{label = _"Yes", ret_val = true})
 	table.insert(options,{label = _"No", ret_val = false})
@@ -181,12 +168,16 @@ end
 
 local function trader_menu(cfg)
 	local debug = false
-	local x = cfg.x
-	local y = cfg.y
 	local items = {}
-	if(type(cfg.items) == "table") then items=cfg.items end  -- we called ourself
-	if(type(cfg.items) == "string") then                     -- we were called by WML
+	local gem_count = {}
+	if(type(cfg.items) == "table") then -- not our first rodeo
+		items=cfg.items
+		gem_count = cfg.gem_count
+	end
+	if(type(cfg.items) == "string") then -- first time we have been called
 		items=loti.item.type.numbers_between(521,530)  -- Add gems.  Looking at ALL items for sort=gem might be wiser.
+		for _,i in pairs(items) do gem_count[i] = wml.variables[loti.item.type[i].wml_var] end
+		cfg.gem_count = gem_count
 		for w in string.gmatch(cfg.items, "[%d]+") do table.insert(items,tonumber(w)) end
 	end
 	local unique_id=cfg.unique_id  -- Unique id for this trader, used to avoid purchasing same item more than once per trader
@@ -194,12 +185,10 @@ local function trader_menu(cfg)
 	local bought_items = {}
 	if (cfg.bought_items ~= nil) then bought_items = cfg.bought_items end
 	local gold = cfg.gold
-	local side_number = cfg.side_number
-	print("Gold = " .. gold)
 
 	-- Menu items, as expected by gui.show_menu()
 	local menu_items = {}
-	table.insert(menu_items, {image="",use_markup="yes",details="<span weight='bold'>Nothing</span>",selected_item=nil,allowed=true})
+	table.insert(menu_items, {image="",use_markup="yes",details="<span weight='bold'>Nothing</span>",description="Get me outta here!",flavour="chocolate",selected_item=nil,allowed=true})
 
 	for _,id in pairs(items) do
 		(function()
@@ -219,11 +208,12 @@ local function trader_menu(cfg)
 					if (bought_it) then return end
 				end
 			end
-			if (debug) then print(string.format("id = %d, name = %s, cost = %d, image = %s",id,item.name,item.cost,item.image)) end
+			if (debug) then print(string.format("id = %d, name = %s, cost = %d, image = %s, desc = %s, flav = %s",
+				id,item.name,item.cost,item.image,item.description,item.flavour)) end
 
-			local label = item.name .. "(" .. item.cost .. _ " gold" .. ")"
+			local label = item.name .. " (" .. item.cost .. _ " gold" .. ")"
 			if (item.sort == "gem") then
-				local have = wml.variables[item.wml_var]
+				local have = gem_count[item.number]
 				if (have == nil) then have=0 end
 				label=item.offer .. " " .. tostring(item.cost) .. _ " (currently have " .. have .. _ " of them)"
 			end
@@ -232,12 +222,21 @@ local function trader_menu(cfg)
 			if (item.cost > gold) then allowed = false end
 
 			local image=item.image
-			if (item.sort == "gem") then image = item.image .. "~CROP(18,18,36,36)" end
+			local description=item.description
+			local flavour=item.flavour
+			if (item.sort == "gem") then
+				image = item.image .. "~CROP(18,18,36,36)"
+				description = item.description_override
+				flavour=""
+			end
+
 
 			-- Might want to check here if item already in menu_items and return if so.
 			table.insert(menu_items, {
 				-- Fields with the same meaning as in gui.show_menu()
 				image = image,
+				description = description,
+				flavour = flavour,
 				use_markup = false,
 				details = label,
 				-- This field is used to determine which item has been selected.
@@ -273,6 +272,7 @@ local function trader_menu(cfg)
 					id = "item_name",
 					text_alignment = "left"
 				}
+
 			}
 		}
 	}
@@ -296,6 +296,7 @@ local function trader_menu(cfg)
 		wml.tag.tooltip { id = "tooltip_large" },
 		wml.tag.helptip { id = "tooltip_large" },
 		wml.tag.grid {
+
 			-- Header (1 row containing a [label] with help)
 			wml.tag.row {
 				wml.tag.column {
@@ -316,14 +317,16 @@ local function trader_menu(cfg)
 
 			-- OK button
 			wml.tag.row {
-			wml.tag.column {
-					border = "top",
-					border_size = 10,
-					wml.tag.button { id = "ok", label = _"OK" },
+				wml.tag.row {
+					wml.tag.column {
+						border = "all",
+						border_size = 20,
+						wml.tag.button { id = "ok", label = _"OK" },
+						}
+					}
 				}
 			}
 		}
-	}
 	local function get_selected_menu_item(dialog)
 		local index = dialog[listbox_id].selected_index
 		return menu_items[index]
@@ -334,8 +337,10 @@ local function trader_menu(cfg)
 
 		for index, menu_item in ipairs(menu_items) do
 			listbox[index].image.label = menu_item.image
+			listbox[index].image.tooltip = menu_item.flavour
 			listbox[index].item_name.use_markup = menu_item.use_markup
 			listbox[index].item_name.label = menu_item.details
+			listbox[index].item_name.tooltip = menu_item.description
 
 			if not menu_item.allowed then
 				listbox[index].item_name.enabled = false
@@ -356,7 +361,10 @@ local function trader_menu(cfg)
 		selected_menu_item = get_selected_menu_item(dialog)
 	end
 
-	gui.show_dialog(dialogDefinition, preshow, postshow)
+	local function gui_show_dialog_no_args()
+		gui.show_dialog(dialogDefinition, preshow, postshow)
+	end
+	wesnoth.synchronize_choice(gui_show_dialog_no_args)
 
 	if not selected_menu_item.allowed then
 		-- Disabled menu item was somehow selected,
@@ -366,14 +374,15 @@ local function trader_menu(cfg)
 
 	if (selected_menu_item.selected_item ~= nil) then
 		local item=loti.item.type[selected_menu_item.selected_item]
-		if (item.sort == "gem") then  -- is this okay in MP?
-			local var=wml.variables[item.wml_var]
-			var = var + 1
-			wml.variables[item.wml_var] = var
-			wml.fire("gold",{side=side_number,amount=-item.cost})
+		if (item.sort == "gem") then
+			gem_count[item.number] = gem_count[item.number]  + 1
+			cfg.gem_count = gem_count
 			gold = gold - item.cost
 			cfg.gold = gold
-			return trader_menu(cfg)  -- stay in menu until player chooses "Nothing"
+			table.insert(bought_items,item)
+			cfg.bought_items = bought_items
+			cfg.items = items
+			return trader_menu(cfg)  -- stay in menu until player chooses "Nothing", but skip confirmation for gems
 		end
 
 		-- Do you really want it?
@@ -383,15 +392,35 @@ local function trader_menu(cfg)
 		table.insert(bought_items,item)
 		gold = gold - item.cost
 
-		if (item.sort ~= "gem") then -- item cannot be purchased more than once at this trader
-			local bought="trader_buys."  .. unique_id .. "." .. selected_menu_item.selected_item
-			wml.variables[bought]="yes"
-		end
+		local bought="trader_buys."  .. unique_id .. "." .. selected_menu_item.selected_item
+		wml.variables[bought]="yes"
 		cfg.bought_items = bought_items
 		cfg.gold = gold
+		cfg.items = items
+
 		return trader_menu(cfg)  -- stay in menu until player chooses "Nothing"
         end
 	return bought_items
+end
+function wesnoth.wml_actions.trader_menu_safe(cfg)  -- and broke
+	local newcfg={}
+	for index, value in pairs(cfg) do
+		newcfg[index] = value
+	end
+	local side_number = wml.variables.side_number
+	local gold = wesnoth.sides.get(side_number).gold
+	newcfg.gold=gold
+
+	local function trader_menu_no_arg()
+		return trader_menu(newcfg)
+	end
+	local bought_items = wesnoth.synchronize_choice(trader_menu_no_arg)
+
+	for _, item in pairs(bought_items) do
+		wml.fire("gold",{side=side_number,amount=-item.cost})
+		loti.item.on_the_ground.add(item.number, cfg.x, cfg.y, item.sort)  -- place item on ground
+		wesnoth.wml_actions.fire_event{ name="item_pick", { "primary_unit", { x=cfg.x, y=cfg.y } } }
+	end
 end
 
 function wesnoth.wml_actions.trader_menu(cfg)
@@ -399,22 +428,17 @@ function wesnoth.wml_actions.trader_menu(cfg)
 	for index, value in pairs(cfg) do
 		newcfg[index] = value
 	end
-	local side_number = wesnoth.sides.find{ wml.tag.has_unit { x=cfg.x,y=cfg.y }}[1].side
+	local side_number = wml.variables.side_number
 	local gold = wesnoth.sides.get(side_number).gold
-	print(type(side_number))
-	print(type(gold))
 	newcfg.gold=gold
-	newcfg.side_number = side_number
 
-	--local bought_items=wesnoth.synchronize_choice(trader_menu(newcfg))
-	local bought_items=wesnoth.synchronize_choice(trader_menu)
-	--local bought_items=trader_menu(newcfg)
+	local bought_items=trader_menu(newcfg)
 
 	for _, item in pairs(bought_items) do
-		--print("I bought " .. item.name)
 		wml.fire("gold",{side=side_number,amount=-item.cost})
 		loti.item.on_the_ground.add(item.number, cfg.x, cfg.y, item.sort)  -- place item on ground
 		wesnoth.wml_actions.fire_event{ name="item_pick", { "primary_unit", { x=cfg.x, y=cfg.y } } }
 	end
 end
+
 
